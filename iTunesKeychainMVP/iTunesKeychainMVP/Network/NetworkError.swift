7c155f197acbdd4af163c97e5affb6aa7c3f3ac6
ronesta <gvozzzd123@gmail.com>
2025-01-10 20:49:32 +0300

    Implementation of architecture MVC

---
+++ iTunesKeychainMVP/iTunesKeychainMVP/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  iTunesKeychainMVP
+//
+//  Created by Ибрагим Габибли on 10.01.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
